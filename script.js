75f6409983d7c459e5faccb6645bf739c69d28d6
LZTTT <tacio.zava@hotmail.com>
2021-06-08 18:26:10 -0300

    definir movimento da corbinha

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "rigth";
+
 
 
 function criarBG(){
     context.fillStyle = "lightgreen"
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
+
+
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
@@ -21,5 +25,29 @@ function criarCobrinha(){
 }
 
 
-criarBG();
-criarCobrinha()
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "rigth") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+
+let jogo = setInterval(iniciarJogo, 100);
