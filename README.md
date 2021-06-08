29b39e49581bf106fbedf9984d968934cd71ecb5
LZTTT <tacio.zava@hotmail.com>
2021-06-08 20:59:13 -0300

    layout finalizado

---
+++ README.md
@@ -0,0 +1,16 @@
+# Oi, tudo bem? Se você está aqui é porque tem interesse em ver meu projeto, certo? :thinking:
+
+
+
+Neste repositório eu usei todo meu conhecimento em JS para replicar o famoso "jogo da cobrinha".
+
+#### Os requisitos necessários para entender os códigos são:
+
+- HTML básico
+- CSS básico
+- Java Script básico
+
+
+
+### :rocket: Vamos lá! :rocket:
+
--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let food = {
 
 
 function criarBG(){
-    context.fillStyle = "lightgreen"
+    context.fillStyle = "black"
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
@@ -53,7 +53,7 @@ function iniciarJogo(){
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
         clearInterval(jogo);
-       alert("GAME OVER :( ")
+       alert("OPS, FIM DE JOGO! BOA SORTE NA PRÓXIMA VEZ")
         }
     }
 
